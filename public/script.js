c045c12618fde8955fab331a9b26766f959aec79
Clark Rabe <45646499+ClarkRabe@users.noreply.github.com>
2023-10-23 22:49:10 +0000

    version 1.0

--- public/script.js
+++ public/script.js
@@ -1,1 +1,51 @@
 console.log('👍 JS is OK!');
+
+const allTheMonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  const allTheDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  
+ 
+  const dateNow = new Date();
+  console.log(dateNow);
+  
+  const gotMonth = dateNow.getMonth();
+
+  let gotDayOfMonth = dateNow.getDate();
+  
+  const gotYear = dateNow.getFullYear();
+ 
+  const gotDayOfWeek = dateNow.getDay();
+  
+  console.log(gotMonth, gotDayOfMonth, gotYear, gotDayOfWeek);
+  
+  const month = document.querySelector(".month");
+  const dayOfMonth = document.querySelector(".dayOfMonth");
+  const year = document.querySelector(".year");
+  const dayOfWeek = document.querySelector(".dayOfWeek");
+  
+
+  month.innerText = allTheMonths[gotMonth];
+  dayOfMonth.innerText = gotDayOfMonth;
+  year.innerText = gotYear;
+  dayOfWeek.innerText = allTheDays[gotDayOfWeek];
+  
